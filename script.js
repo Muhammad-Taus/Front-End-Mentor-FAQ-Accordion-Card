1c4e4309eba653a25145c9cf88054fc6ceb762aa
Muhammad Taus <md.taus03@gmail.com>
2024-10-15 11:44:39 +0600

    fixing smooth scroll accordion

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const accordion = document.querySelector(".accordion h3");
-
-// for(i=0; i<accordion.length ; i++){
-//     accordion[i].addEventListener("click", ()=> {
-//         this.classList.toggle("active");
-//     })
-// }
+const accordions = document.querySelectorAll(".accordion h3");
 
 
+accordions.forEach(accordion => {
+    accordion.addEventListener("click", () => {
+        
+        const accordionText = accordion.nextElementSibling;
+        
+        console.log(accordionText);
+        accordionText.classList.toggle("active");
+        accordion.classList.toggle("active");
 
-accordion.addEventListener("click", (e) => {
-    alert("accprdion clicked!");
-    console.log(accordion);
-    this.classList.toggle("active");
-})
+    });
+});
