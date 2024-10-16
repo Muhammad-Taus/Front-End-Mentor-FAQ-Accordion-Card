37ca5b5dd5ab825bcd81ca4a18260983c7ca05be
Muhammad Taus <md.taus03@gmail.com>
2024-10-16 22:03:48 +0600

    mobile done

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const accordionTitles = document.querySelectorAll(".accordion-title");
 
 accordionTitles.forEach(accordionTitle => {
     accordionTitle.addEventListener("click", () => {
-        
+
         const accordionText = accordionTitle.nextElementSibling;
-        
+
         accordionText.classList.toggle("active");
         accordionTitle.childNodes[3].classList.toggle("active");
-        
+
         // console.log(accordionTitle.childNodes);
         // console.log(accordionTitle.childNodes[3]);
         // console.log(accordionText);
